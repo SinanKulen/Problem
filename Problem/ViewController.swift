aabca574e59b764a114754f2d291d3e254883acb
SinanKulen <sinan.kulen@gmail.com>
2021-07-13 01:54:28 +0300

    Problem

--- Problem/ViewController.swift
+++ Problem/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textWeight: UITextField!
+    @IBOutlet weak var labelResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClick(_ sender: UIButton) {
+       
+        labelResult.text = String(Int(textWeight.text!)! * 10)
+        
+    }
+    
 }
 
